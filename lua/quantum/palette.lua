9b532c16c2256a84171165f07ce910f9b174e5fd
kjssad <kimsadomia@protonmail.com>
2021-11-30 08:14:47 +0800

    feat(palette): add color definitions

---
+++ lua/quantum/palette.lua
@@ -0,0 +1,38 @@
+return {
+  fg = "#d7d7db",
+  fg_sec = "#c3c3c6",
+  fg_alt = "#939395",
+  fg_gutter = "#4a4a4f",
+
+  bg = "#2a2a2e",
+  bg_sec = "#303034",
+  bg_alt = "#38383d",
+  bg_dark = "#232327",
+  bg_black = "#1b1b1d",
+
+  comment = "#737373",
+  search = "#58585C",
+  guides = "#414145",
+
+  blue = "#6b89ff",
+  cyan = "#75bfff",
+  green = "#86de74",
+  magenta = "#ff8de9",
+  orange = "#fc9867",
+  purple = "#b98eff",
+
+  ui_cyan = "#48acf2",
+  ui_green = "#6cca69",
+  ui_red = "#f25e72",
+  ui_yellow = "#ffd866",
+  ui_purple = "#b058dd",
+
+  git_yellow = "#e0b17f",
+  git_green = "#29aa84",
+  git_red = "#e6668b",
+
+  diff_yellow = "#363130",
+  diff_green = "#243130",
+  diff_red = "#362930",
+  diff_text = "#4f443b",
+}
